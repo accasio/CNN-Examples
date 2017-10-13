9c24d7d226873ec0c9c5a8a848aead7d369f9db1
accasio <accasio@gmail.com>
2017-10-13 16:21:59 +0100

    added tensorflow script

---
+++ tf1/utils.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import csv
+import tensorflow as tf
+
+def readTrafficSigns(rootpath):
+    '''Reads traffic sign data for German Traffic Sign Recognition Benchmark.
+
+    Arguments: path to the traffic sign data, for example './GTSRB/Training'
+    Returns:   list of images, list of corresponding labels'''
+    images = []  # images
+    labels = []  # corresponding labels
+    # loop over all 42 classes
+    for c in range(0, 43):
+        prefix = rootpath + '/' + format(c, '05d') + '/'  # subdirectory for class
+        gtFile = open(prefix + 'GT-' + format(c, '05d') + '.csv')  # annotations file
+        gtReader = csv.reader(gtFile, delimiter=';')  # csv parser for annotations file
+        next(gtReader)  # skip header
+        # loop over all images in current annotations file
+        for row in gtReader:
+            images.append(plt.imread(prefix + row[0]))  # the 1th column is the filename
+            labels.append(row[7])  # the 8th column is the label
+        gtFile.close()
+    return images, labels
+
+
+trainImages, trainLabels = readTrafficSigns('c:\\users\\accas\\pictures\\GTSRB\\Training\\Images')
+print(len(trainLabels), len(trainImages))
+print((trainLabels[0]))
+plt.imshow(trainImages[42])
+plt.show()
+
